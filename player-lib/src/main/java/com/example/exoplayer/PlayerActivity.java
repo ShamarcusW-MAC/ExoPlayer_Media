d00807f1cddaa46c61201167747a242a9f5f6852
ShamarcusW-MAC <56922234+ShamarcusW-MAC@users.noreply.github.com>
2020-01-22 18:56:50 -0500

    Customized UI

--- player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
+++ player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -17,16 +17,176 @@ package com.example.exoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Log;
+import com.google.android.exoplayer2.util.Util;
 
 
 public class PlayerActivity extends AppCompatActivity {
+
+  private PlayerView playerView;
+  private SimpleExoPlayer player;
+  private boolean playWhenReady = true;
+  private int currentWindow = 0;
+  private long playbackPosition = 0;
+  private PlaybackStateListener playbackStateListener;
+  private static final String TAG = PlayerActivity.class.getName();
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_player);
+
+    playbackStateListener = new PlaybackStateListener();
+    playerView = findViewById(R.id.video_view);
   }
 
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    if (Util.SDK_INT >= 24) {
+      initializePlayer();
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    hideSystemUi();
+    if ((Util.SDK_INT < 24 || player == null)) {
+      initializePlayer();
+    }
+  }
+
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    if (Util.SDK_INT < 24) {
+      releasePlayer();
+    }
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    if (Util.SDK_INT >= 24) {
+      releasePlayer();
+    }
+  }
+
+
+  @SuppressLint("InlinedApi")
+  private void hideSystemUi() {
+    playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+  }
+
+  private void initializePlayer() {
+//    player = ExoPlayerFactory.newSimpleInstance(this);
+    if (player == null) {
+      DefaultTrackSelector trackSelector = new DefaultTrackSelector();
+      trackSelector.setParameters(
+              trackSelector.buildUponParameters().setMaxVideoSizeSd());
+      player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+    }
+    playerView.setPlayer(player);
+//    Uri uri = Uri.parse(getString(R.string.media_url_mp4));
+    Uri uri = Uri.parse(getString(R.string.media_url_dash));
+    MediaSource mediaSource = buildMediaSource(uri);
+    player.setPlayWhenReady(playWhenReady);
+    player.seekTo(currentWindow, playbackPosition);
+//    player.prepare(mediaSource, false, false);
+    player.addListener(playbackStateListener);
+    player.prepare(mediaSource, false, false);
+
+
+  }
+
+  private void releasePlayer() {
+    if (player != null) {
+      playWhenReady = player.getPlayWhenReady();
+      playbackPosition = player.getCurrentPosition();
+      currentWindow = player.getCurrentWindowIndex();
+      player.removeListener(playbackStateListener);
+      player.release();
+      player = null;
+    }
+  }
+
+  private MediaSource buildMediaSource(Uri uri) {
+//    DataSource.Factory dataSourceFactory =
+//            new DefaultDataSourceFactory(this, "exoplayer-codelab");
+//    return new ProgressiveMediaSource.Factory(dataSourceFactory)
+//            .createMediaSource(uri);
+//------------------------------------------------------------------------------------
+    // These factories are used to construct two media sources below
+//    DataSource.Factory dataSourceFactory =
+//            new DefaultDataSourceFactory(this, "exoplayer-codelab");
+//    ProgressiveMediaSource.Factory mediaSourceFactory =
+//            new ProgressiveMediaSource.Factory(dataSourceFactory);
+//
+//    // Create a media source using the supplied URI
+//    MediaSource mediaSource1 = mediaSourceFactory.createMediaSource(uri);
+//
+//    // Additionally create a media source using an MP3
+//    Uri audioUri = Uri.parse(getString(R.string.media_url_mp3));
+//    MediaSource mediaSource2 = mediaSourceFactory.createMediaSource(audioUri);
+//
+//    return new ConcatenatingMediaSource(mediaSource1, mediaSource2);
+
+    DataSource.Factory dataSourceFactory =
+            new DefaultDataSourceFactory(this, "exoplayer-codelab");
+    DashMediaSource.Factory mediaSourceFactory = new DashMediaSource.Factory(dataSourceFactory);
+    return mediaSourceFactory.createMediaSource(uri);
+  }
+
+  private class PlaybackStateListener implements Player.EventListener {
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady,
+                                     int playbackState) {
+      String stateString;
+      switch (playbackState) {
+        case ExoPlayer.STATE_IDLE:
+          stateString = "ExoPlayer.STATE_IDLE      -";
+          break;
+        case ExoPlayer.STATE_BUFFERING:
+          stateString = "ExoPlayer.STATE_BUFFERING -";
+          break;
+        case ExoPlayer.STATE_READY:
+          stateString = "ExoPlayer.STATE_READY     -";
+          break;
+        case ExoPlayer.STATE_ENDED:
+          stateString = "ExoPlayer.STATE_ENDED     -";
+          break;
+        default:
+          stateString = "UNKNOWN_STATE             -";
+          break;
+      }
+      Log.d(TAG, "changed state to " + stateString
+              + " playWhenReady: " + playWhenReady);
+    }
+  }
 }
